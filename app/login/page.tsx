ba7aa2b9f5fbcae457519b260d39c2c128518d2a
Yosh <imyosh4@gmail.com>
2024-01-04 19:31:34 +0300

    login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,13 +8,16 @@ export default async function page() {
   if (session) redirect("/");
 
   return (
-    <main className="flex login-background -mx-4 -mb-6 flex-grow justify-center items-center flex-col gap-6">
-      <h1 className="text-xl border tracking-[1px] p-4 rounded-full w-20 h-20 flex items-center justify-center">
-        PIPY
-      </h1>
-      <p className="text-secondary-foreground text-center mb-8">
-        All in one investors management tool for forex trading.
-      </p>
+    <main className="flex login-background -mx-4 px-4 -mb-6 flex-grow justify-center items-center flex-col gap-12">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-xl border tracking-[1px] p-4 rounded-full w-20 h-20 flex items-center justify-center">
+          PIPY
+        </h1>
+        <p className="text-secondary-foreground text-center mb-8">
+          All in one investors management tool for forex trading.
+        </p>
+      </div>
+
       <GoogleSigninButton />
     </main>
   );
