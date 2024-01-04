8ee5f63df28ea8577da8fb0c8f50fab5776fe8b4
Yosh <imyosh4@gmail.com>
2024-01-04 18:14:21 +0300

    update the login page

--- components/GoogleSigninButton.tsx
+++ components/GoogleSigninButton.tsx
@@ -3,15 +3,30 @@
 import { Button } from "@/components/ui/button";
 import GoogleIcon from "@/public/svg/google.svg";
 import { signIn } from "next-auth/react";
+import Loader from "./Loader";
+import { useState } from "react";
 
 export default function GoogleSigninButton() {
+  const [loading, setLoading] = useState(false);
+
+  const startSignIn = async () => {
+    setLoading(true);
+    await signIn("google", { callbackUrl: "/" });
+    setLoading(false);
+  };
+
   return (
     <Button
-      onClick={() => signIn("google", { callbackUrl: "/" })}
+      disabled={loading}
+      onClick={async () => startSignIn()}
       variant="outline"
-      className="gap-2"
+      className="gap-4 h-12 px-5"
     >
-      <GoogleIcon className="w-4" />
+      {loading ? (
+        <Loader isLoading className="w-5" />
+      ) : (
+        <GoogleIcon className="w-5" />
+      )}
       Sign In with google
     </Button>
   );
