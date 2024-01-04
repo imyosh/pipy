c12b01992a2e12b37656a62dab44c316e6246408
Yosh <imyosh4@gmail.com>
2024-01-04 19:34:49 +0300

    login space

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,12 +8,12 @@ export default async function page() {
   if (session) redirect("/");
 
   return (
-    <main className="flex login-background -mx-4 px-4 -mb-6 flex-grow justify-center items-center flex-col gap-12">
+    <main className="flex login-background -mx-4 px-4 -mb-6 flex-grow justify-center items-center flex-col gap-[17rem]">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-xl border tracking-[1px] p-4 rounded-full w-20 h-20 flex items-center justify-center">
           PIPY
         </h1>
-        <p className="text-secondary-foreground text-center mb-8">
+        <p className="text-secondary-foreground text-center">
           All in one investors management tool for forex trading.
         </p>
       </div>
