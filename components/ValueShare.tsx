06addea72deda6a5629aa220b1bb9e82a5c833b3
Yosh <imyosh4@gmail.com>
2024-02-04 16:43:47 +0300

    %

--- components/ValueShare.tsx
+++ components/ValueShare.tsx
@@ -3,7 +3,7 @@ import ValueShareOptions from "./ValueShareOptions";
 
 export default function ValueShare({ share }: { share: ValueShareType }) {
   return (
-    <div className="relative flex flex-1 flex-col gap-2 rounded-xl border p-4">
+    <div className="relative flex flex-1 flex-col gap-2 rounded-xl border p-3">
       <h2 className="text-xs font-bold capitalize text-secondary-foreground">
         {share.title}
       </h2>
@@ -12,6 +12,12 @@ export default function ValueShare({ share }: { share: ValueShareType }) {
         <span className="text-xl">$</span>
         {share.value.toFixed(2)}
       </p>
+
+      {share.handler !== "mine" && (
+        <p className="text-[0.6rem] z-20 text-green-200 bottom-3 right-3 absolute">
+          {share.percentage * 100}%
+        </p>
+      )}
     </div>
   );
 }
